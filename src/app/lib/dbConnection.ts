d67093bb49350db2755e1c4beb9a6a7e2ae4f92a
Juan José Rodriguez <zoranbow@gmail.com>
2024-04-27 16:24:19 -0500

    mongoDB Connection & @t3-oss/env-nextjs

---
+++ src/app/lib/dbConnection.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+import { env } from '@/env';
+
+const connection: { isConnected?: number } = {};
+
+async function dbConnect(): Promise<void> {
+  if (connection.isConnected) {
+    return;
+  }
+
+  const db = await mongoose.connect(env.MONGODB_URI);
+
+  connection.isConnected = db.connections[0].readyState;
+}
+
+export default dbConnect;
